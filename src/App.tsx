798ecac2073d4100def4756d2647d6da8ce98e1f
cococheong07 <104583910+cococheong07@users.noreply.github.com>
2022-04-29 11:52:24 +0800

    new
    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import DoughnutChart from "./Componet/Chart/DoughnutChart";
+// import SiteNavBar from "./Componet/SiteNavBar";
+// import CrazyChart from "./Componet/Chart/CrazyChart";
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/*<SiteNavBar/>*/}
+        <DoughnutChart/>
+        {/*<CrazyChart/>*/}
     </div>
   );
 }
---
+++ src/Componet/Chart/CrazyChart/index.js
@@ -0,0 +1,33 @@
+// import React from "react";
+// import {Bar} from "react-chartjs-2";
+// import data from "bootstrap/js/src/dom/data";
+//
+// function CrazyChart(){
+//     const genData = {
+//         datasets: [{
+//             type: 'bar',
+//             label: 'Bar Dataset',
+//             data: [10, 20, 30, 40]
+//         }, {
+//             type: 'line',
+//             label: 'Line Dataset',
+//             data: [50, 50, 50, 50],
+//         }],
+//         labels: ['January', 'February', 'March', 'April']
+//     }
+//     const config = {
+//         type: 'scatter',
+//         data: data,
+//         options: {
+//             scales: {
+//                 y: {
+//                     beginAtZero: true
+//                 }
+//             }
+//         }
+//     };
+//     return(
+//         <Bar data={'genData'} type={'scatter'} options="config"/>
+//     );
+// }
+// export default CrazyChart;
